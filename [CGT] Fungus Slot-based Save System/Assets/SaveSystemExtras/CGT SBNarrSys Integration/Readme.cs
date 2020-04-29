c3cc8d1fe708d5672d4fe5495d80b3ade92bd35c
CG-Tespy <gabriel.cabrera257@hotmail.com>
2020-04-29 13:27:08 -0400

    Small reorganization of files

---
+++ [CGT] Fungus Slot-based Save System/Assets/SaveSystemExtras/CGT SBNarrSys Integration/Readme.cs
@@ -0,0 +1,10 @@
+﻿/*
+
+This is for integrating my Slot-Based Narrative System with my Slot-Based Save System.
+Only include these scripts if you are using said Narrative System, otherwise they'll
+just add errors to your project.
+
+Github for the Narrative System: 
+https://github.com/CG-Tespy/Fungus-Slot-based-Narrative-System
+
+*/
--- [CGT] Fungus Slot-based Save System/Assets/SaveSystemExtras/CGT SBNarrSys/readme.txt
+++
@@ -1,6 +0,0 @@
-This is for integrating my Slot-Based Narrative System with my Slot-Based Save System.
-Only include these if you are using my Narrative System, otherwise the scripts here will
-cause errors.
-
-Github for the Narrative System: 
-https://github.com/CG-Tespy/Fungus-Slot-based-Narrative-System
