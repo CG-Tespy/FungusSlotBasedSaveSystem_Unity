606ee3840603ea7535f2c717e30e5c642ae27ee7
CG-Tespy <gabriel.cabrera257@hotmail.com>
2020-06-15 18:30:27 -0400

    A bit more testing

--- [CGT] Fungus Slot-based Save System/Assets/SaveSystemExtras/CGT SBNarrSys Integration/Readme.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-
-These integration scripts are made to work with Fungus 3.13's built-in Narrative System, which
-has adapted the one I made (I'm honored, btw, Fungus devs!)
-
-Github for the Narrative System adapted: 
-https://github.com/CG-Tespy/Fungus-Slot-based-Narrative-System
-
-*/
